cc7417691f090b7132292595c02405a7ce4a71b4
bishwajit70 <mailtobishwa15@gmail.com>
2022-04-29 20:35:50 +0600

    simply added all authentication

--- src/components/Pages/SocialLogin/SocialLogin.js
+++ src/components/Pages/SocialLogin/SocialLogin.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { GrGoogle } from 'react-icons/gr';
+import { FaGithub } from 'react-icons/fa';
 
 const SocialLogin = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, googleUser, googleLoading, GoogleError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, githubUser, githubLoading, githubError] = useSignInWithGithub(auth);
+
 
     return (
         <div className='pb-5 w-11/12 mx-auto'>
-            <div className='grid text-left'>
-                <button onClick={() => signInWithGoogle()} className='cursor-pointer duration-700 p-2 uppercase border-2 mb-2 text-xl text-white bg-blue-600 hover:bg-blue-800 border-blue-600 rounded'>Continue With Google</button>
+            <div className='grid'>
+                <button onClick={() => signInWithGoogle()} className='cursor-pointer duration-700 p-2 uppercase border-2 mb-2 text-xl text-white bg-blue-600 hover:bg-blue-800 border-blue-600 rounded'><GrGoogle className='text-wihte text-2xl mr-5 inline-block' />Continue With Google</button>
             </div>
             <div className='grid my-2 text-left'>
-                <button className='cursor-pointer duration-700 p-2 uppercase border-2 mb-2 text-xl text-white bg-blue-600 hover:bg-blue-800 border-blue-600 rounded'>Continue With GitHub</button>
+
+                <button onClick={() => signInWithGithub()} className='cursor-pointer duration-700 p-2 uppercase border-2 mb-2 text-xl text-white bg-blue-600 hover:bg-blue-800 border-blue-600 rounded'>
+                    <FaGithub className='text-wihte text-2xl mr-5 inline-block' />
+                    Continue With GitHub</button>
             </div>
         </div >
     );
