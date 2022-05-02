d31a5df66c1459902827eabb3edbe756446e693f
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-02 15:21:26 +0600

    env local file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,15 +7,27 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyChlh2dsOPK_ynrQGipvF__34NATh4y1z8",
-    authDomain: "warehouse-management-332f6.firebaseapp.com",
-    projectId: "warehouse-management-332f6",
-    storageBucket: "warehouse-management-332f6.appspot.com",
-    messagingSenderId: "980544896912",
-    appId: "1:980544896912:web:c62af7f06a9437832b7d15"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 export default auth;
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyChlh2dsOPK_ynrQGipvF__34NATh4y1z8",
+//     authDomain: "warehouse-management-332f6.firebaseapp.com",
+//     projectId: "warehouse-management-332f6",
+//     storageBucket: "warehouse-management-332f6.appspot.com",
+//     messagingSenderId: "980544896912",
+//     appId: "1:980544896912:web:c62af7f06a9437832b7d15"
+// };
+
+// process.env.REACT_APP_apiKey
