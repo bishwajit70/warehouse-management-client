0ce86bbb130187e2a689e07383886e23aaac7641
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-08 20:27:48 +0600

    Page Not found modified

--- src/components/Pages/NotFoundPage/NotFoundPage.js
+++ src/components/Pages/NotFoundPage/NotFoundPage.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
     return (
-        <div className='grid grid-cols-1 px-40 mx-auto w-full justify-between items-center'>
+        <div className='grid grid-cols-1 md:px-40 mx-auto w-full justify-between items-center'>
             <div className='py-40'>
-                <h1 className='text-6xl font-bold'>Page Not Found</h1>
+                <h1 className='text-3xl md:text-5xl lg:text-6xl xl:text-6xl text-purple-600 font-bold'>Page Not Found</h1>
                 <p className='pt-10 text-xl'>We're sorry, but we can't find the page you were looking for. <Link className='text-purple-600 font-bold' to='/'>Go Home</Link></p>
             </div>
         </div>
